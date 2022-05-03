3251cf1783b3cfa0527d0f7b00e4b63c38d76c46
Vic Gauthreaux <vicgauthreaux@gmail.com>
2022-05-03 08:00:26 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Vic Gauthreaux
+    File Name: script.js
+    Date: Today's date
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.home').height($(window).height());
+});
